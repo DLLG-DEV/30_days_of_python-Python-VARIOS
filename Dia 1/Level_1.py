eb69833aec721ac9de7241577ba5c04bb81626dc
Papi Chulo Admin <davleonardo20@gmail.com.com>
2026-07-10 00:40:21 -0600

    primeros dos dias de python, dia 2 no completo | 10 07 2026 | DLLG

---
+++ Dia 1/Level_1.py
@@ -0,0 +1,33 @@
+#SALUDO
+print("Hello World")
+
+#ADICION
+print('1 + 5 =', 1 + 5)
+
+#SUBTRACCION
+print('6 - 5 = ', 6 - 5)
+
+#MULTIPLICACION
+print('7 * 8 =', 7 * 8)
+
+#MODULO
+print('3 % 2 =', 3 % 2)
+
+#DIVISOIN
+print('10 / 3 =', round((10 / 3), 3))
+
+#ESPONENCIACION
+print('2 ** 5 =', 2**5)
+
+#FLOOR DIVISION
+print('5 // 3 =', 5 // 3)
+
+#TYPES
+print('Type of 10 is', type(10))
+print('Type of 9.5 is', type(9.5))
+print('Type of 3.14 is', type(3.14))
+print('Type of 4 + 4j is', type(4+4j))
+print('Type of ["Asabeneh", "Python", "Finland"] is', type(["Asabeneh", "Python", "Finland"]))
+print('Type of "David Leonardo" is', type("David Leonardo"))
+print('Type of "Lopez´s family" is', type("Lopez´s family"))
+print('Type of "MX" is', type("MX"))
